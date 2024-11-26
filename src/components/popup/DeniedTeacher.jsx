9822271bddfc6f049d9b6a319daf2ef8905f94e2
Emil <emilloengaa@icloud.com>
2024-11-26 09:55:54 +0100

    pop modal

--- src/components/popup/DeniedTeacher.jsx
+++ src/components/popup/DeniedTeacher.jsx
@@ -23,29 +23,27 @@ function deniedTeacher({name}) {
     }
 
     return (
-        <div>
-            <div style={deniedStyle}>
-                
-                <h1 style={{color: "#C92A2A"}}><img style={{width: "50px", height: "50px", marginRight: "20px"}} src="img/no.svg" alt="nægtet mærke" />Bookning bekræftet</h1>
-                <p>For at overskrive bookingen vedr. </p>
-                <p>Lokale: <b>{name}</b></p>
-                <p>Dato: <b>08/12/2024</b> </p>
-                <p>Tidspunkt: <b>13:00 - 15:00</b></p>
-                <p>Skal du kontakte <b>XX (cph-jj123@cphbusiness.dk)</b> selv, da der er under 24 timer til deres booking af lokalet starter. </p>
-                <Link to={"BookLaokale"}>
-                    <Button 
-                        type="submit" 
-                        variant="filled" 
-                        color="#F08C00" 
-                        size="xl" 
-                        radius="md" 
-                        style={buttonStyle}>Tilbage til bookingsiden</Button>
-
-                </Link>
-                
+         <Modal opened={opened} onClose={onClose} title="Authentication">
+            <div>
+                <div style={deniedStyle}>
+                    <h1 style={{color: "#C92A2A"}}><img style={{width: "50px", height: "50px", marginRight: "20px"}} src="img/no.svg" alt="nægtet mærke" />Bookning bekræftet</h1>
+                    <p>For at overskrive bookingen vedr. </p>
+                    <p>Lokale: <b>{name}</b></p>
+                    <p>Dato: <b>08/12/2024</b> </p>
+                    <p>Tidspunkt: <b>13:00 - 15:00</b></p>
+                    <p>Skal du kontakte <b>XX (cph-jj123@cphbusiness.dk)</b> selv, da der er under 24 timer til deres booking af lokalet starter. </p>
+                    <Link to={"BookLaokale"}>
+                        <Button 
+                            type="submit" 
+                            variant="filled" 
+                            color="#F08C00" 
+                            size="xl" 
+                            radius="md" 
+                            style={buttonStyle}>Tilbage til bookingsiden</Button>
+                    </Link>
+                </div>
             </div>
-        </div>
-        
+        </Modal>
     )
 }
 
--- src/components/popup/confirm.jsx
+++ src/components/popup/confirm.jsx
@@ -32,30 +32,28 @@ function confirm({name}) {
     }
 
     return (
-        <div style={centerBackground}>
-            <div style={confirmStyle}>
-                
-                <h1 style={{color: "#2B8A3E"}}><img style={{width: "50px", height: "50px", marginRight: "20px"}} src="img/check.svg" alt="" />Bookning bekræftet</h1>
-                <p>Lokale: <b>{name}</b></p>
-                <p>Dato: <b>08/12/2024</b></p>
-                <p>Tidspunkt: <b>13:00 - 15:00</b></p>
-                <p>Vær opmærksom på at hvis du er studerende kan få din booking overskrevet  af en underviser, hvis der er mere end 24 timer til din booking. Du vil få en mail med begrundelse for aflysningen. </p>
-                <br></br>
-                <p>Er der under 24 timer til din booking og en underviser vil booke lokalet, vil du få en mail med en anmodning om at overtage bookingen. Du må selv bestemme om du vil beholde eller overgive bookingen.</p>
-                <Link to={"/ownBooking"}>
+         <Modal opened={opened} onClose={onClose} title="Authentication">
+            <div style={centerBackground}>
+                <div style={confirmStyle}>
+                    <h1 style={{color: "#2B8A3E"}}><img style={{width: "50px", height: "50px", marginRight: "20px"}} src="img/check.svg" alt="" />Bookning bekræftet</h1>
+                    <p>Lokale: <b>{name}</b></p>
+                    <p>Dato: <b>08/12/2024</b></p>
+                    <p>Tidspunkt: <b>13:00 - 15:00</b></p>
+                    <p>Vær opmærksom på at hvis du er studerende kan få din booking overskrevet  af en underviser, hvis der er mere end 24 timer til din booking. Du vil få en mail med begrundelse for aflysningen. </p>
+                    <br></br>
+                    <p>Er der under 24 timer til din booking og en underviser vil booke lokalet, vil du få en mail med en anmodning om at overtage bookingen. Du må selv bestemme om du vil beholde eller overgive bookingen.</p>
+                    <Link to={"/ownBooking"}>
                     <Button 
                         type="submit" 
                         variant="filled" 
                         color="#F08C00" 
                         size="xl" 
-                        radius="md" 
+                        adius="md" 
                         style={buttonStyle}>Til forsiden</Button>
-
-                </Link>
-                
+                    </Link>
+                </div>
             </div>
-        </div>
-        
+        </Modal>
     )
 }
 
