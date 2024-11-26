e814746dd9bde0312ce2444accf4818ac2e2664a
SophiaAnina <148782071+SophiaAnina@users.noreply.github.com>
2024-11-26 14:48:22 +0100

    LokaleBillederV.2

--- src/components/booklokale/lokaleBillede.jsx
+++ src/components/booklokale/lokaleBillede.jsx
@@ -1,35 +1,55 @@
+
 import { useState } from "react";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 
-function LokaleBilleder ({}) {
+function LokaleBilleder({ lokale }) {
     const [currentIndex, setCurrentIndex] = useState(0);
-  const images = [
-    "lokale3-9.jpg",
-    "lokale3-9-2.jpg",
-    "lokale3-9-3.jpg",
-  ];
 
-  const handleNext = () => {
-    setCurrentIndex((currentIndex + 1) % images.length);
-  };
+    const lokaleImages = {
+        "3.9 Open Learning": [
+            "lokale3-9.jpg",
+            "lokale3-9-2.jpg",
+            "lokale3-9-3.jpg",
+        ],
+        "3.2": [
+            "lokale3-2.jpg",
+            "lokale3-2-2.jpg",
+        ],
+    };
 
-  const handlePrev = () => {
-    setCurrentIndex((currentIndex - 1 + images.length) % images.length);
-  };
+    const images = lokaleImages[lokale] || [];
 
-    
-    
+    const handleNext = () => {
+        setCurrentIndex((currentIndex + 1) % images.length);
+    };
+
+    const handlePrev = () => {
+        setCurrentIndex((currentIndex - 1 + images.length) % images.length);
+    };
+
+    if (images.length === 0) {
+        return <div> </div>;
+    }
+
     return (
-    <div>
-        <div style={{width: "300px",height: "200px",display: "flex", overflow: "hidden",}}>
-        <img style={{ width: "300px" , height: "200px"}} src={images[currentIndex]} alt="" />
+        <div>
+            <div style={{ width: "300px", height: "200px", display: "flex", overflow: "hidden" }}>
+                <img
+                    style={{ width: "300px", height: "200px" }}
+                    src={images[currentIndex]}
+                    alt={`Image of lokale ${lokale}`}
+                />
+            </div>
+            <div style={{ display: "flex", justifyContent: "space-between", width: "300px" }}>
+                <button onClick={handlePrev}>
+                    <FaChevronLeft />
+                </button>
+                <button onClick={handleNext}>
+                    <FaChevronRight />
+                </button>
+            </div>
         </div>
-        <div style={{display:"flex", justifyContent:"space-between", width:"300px"}}>
-        <button onClick={handlePrev}> <FaChevronLeft />  </button>
-        <button onClick={handleNext}> <FaChevronRight /> </button>
-        </div>
-        </div>
-    )
+    );
 }
 
-export default LokaleBilleder
+export default LokaleBilleder;
